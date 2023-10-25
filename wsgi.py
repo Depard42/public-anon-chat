a6e8ca8029958e14e9011bcabb06e8f21ca847ce
Depard42 <tyomaduobakh@gmail.com>
2023-10-25 19:56:18 +0300

    -ch. wsgi

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ HOST="127.0.0.0"
 PORT=8124
 
 if __name__ == "__main__":
-    socketio.run(app, debug=False, host=HOST, port=PORT)
+    socketio.run(app, debug=False, host=HOST, port=PORT, allow_unsafe_werkzeug=True)
