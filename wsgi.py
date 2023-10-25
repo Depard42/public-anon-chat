c30a508c9197984a05295b9e6fa6fc4aed444b94
Depard42 <tyomaduobakh@gmail.com>
2023-10-25 23:43:02 +0300

    -proxy fix

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 from app.app import app, socketio
 HOST="0.0.0.0"
 PORT=8124
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 if __name__ == "__main__":
     socketio.run(app, debug=False, host=HOST, port=PORT, allow_unsafe_werkzeug=True)
